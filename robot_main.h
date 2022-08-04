5ec154bcb7ec4a9bae41ebc85a67a833c24d79dd
Schneison <larson.schneider11@gmail.com>
2022-08-05 01:32:08 +0200

    Compress image

--- robot_main.h
+++ robot_main.h
@@ -13,13 +13,14 @@
 #ifndef ROBOT_MAIN_H
 #define ROBOT_MAIN_H
 
+#include <stdlib.h>
+
 #include "timers.h"
 #include "usart.h"
 #include "robot_sensor.h"
 #include "drive_control.h"
 #include "state_control.h"
 #include "led_control.h"
-#include <stdlib.h>
 
 /**
  * @brief Setup board registries
